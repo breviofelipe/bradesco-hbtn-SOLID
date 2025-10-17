cafc3cd8b0eb1190216e366f2da29601da8ffc23
Felipe Brevio <felipebrevio05@gmail.com>
2025-10-17 20:24:28 -0300

    feat

---
+++ 0x04/convencao/UserModel.java
@@ -0,0 +1,23 @@
+// classe users, representando nossa entidade
+
+class UsersModels {
+
+    int idUser;
+
+    String descricao;
+
+    final static int IDADE_MINIMA = 18;
+
+// função que coleta todos os users
+
+    public getUsers() {
+        return [];
+    }
+
+// função que coleta todos os users
+
+    public getAllUsers() {
+        return [];
+    }
+
+}
